613ead5410a093d46a0b02daca58307874e44545
cleiva <crisjosleiva@gmail.com>
2020-09-16 18:03:15 -0400

    added embedding code but needs refactoring

--- standalone-videogameapp/src/components/Unity/UnityComponent.jsx
+++ standalone-videogameapp/src/components/Unity/UnityComponent.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
-class UnityComponent extends React.Component {
-    render = () => {
-        return (
-            <iframe
-                frameborder="0"
-                src="https://itch.io/embed-upload/2717928?color=333333"
-                allowfullscreen=""
-                height="580px"
-                width="100%"
-            
-            >
-                <a href="https://nelsonwang.itch.io/afreebird-videogame">
-                Play afreebird-videogame on itch.io
-                </a>
-            </iframe>
-        )
-    }
+const frame = '<iframe frameBorder=0 src="https://itch.io/embed-upload/2717928?color=333333" allowFullScreen="" height="580px" width="100%" '
+
+const IFrame = (props) => {
+    return (
+        <div dangerouslySetInnerHTML={ {__html: props.iframe?props.iframe: ''}} />
+    )
+};
+
+const UnityComponent = () => {
+    return (
+        <div className='unity'>
+            <iframe src="https://itch.io/embed-upload/2717928?color=333333" />
+        </div>
+    )
 };
 
 export default UnityComponent;
