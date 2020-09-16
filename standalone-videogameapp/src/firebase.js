2f06058b86254830aa0287e4afed30cd662089ff
cleiva <crisjosleiva@gmail.com>
2020-09-16 17:11:18 -0400

    added a more secure and correct way to handle the config

--- standalone-videogameapp/src/firebase.js
+++ standalone-videogameapp/src/firebase.js
@@ -1,7 +1,21 @@
 import firebase from 'firebase';
 
-const config = {
+const apiKey = process.env.REACT_APP_APIKEY
+const authDomain = process.env.REACT_APP_AUTHDOMAIN
+const databaseURL = process.env.REACT_APP_DATABASE_URL
+const projectID = process.env.REACT_APP_PROJECT_ID
+const storageBucket = process.env.REACT_APP_STORAGE_BUCKET
+const messagingSenderID = process.env.REACT_APP_MESSAGING_SENDER_ID
+const appID = process.env.REACT_APP_APP_ID
 
+const config = {
+    apiKey: `${apiKey}`,
+    authDomain: `${authDomain}`,
+    databaseURL: `${databaseURL}`,
+    projectId: `${projectID}`,
+    storageBucket: `${storageBucket}`,
+    messagingSenderId: `${messagingSenderID}`,
+    appId: `${appID}`
 }
 
 firebase.initializeApp(config);
