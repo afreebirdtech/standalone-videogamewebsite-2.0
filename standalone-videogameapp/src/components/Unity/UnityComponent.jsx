6f1e8e62384b0e032b6c777fe3c3e0be62dd92d6
Cristian Leiva <crisjosleiva@gmail.com>
2020-11-23 15:20:00 -0500

    seperated itchio embed to its own func

--- standalone-videogameapp/src/components/Unity/UnityComponent.jsx
+++ standalone-videogameapp/src/components/Unity/UnityComponent.jsx
@@ -6,13 +6,22 @@ import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed';
 class UnityComponent extends React.Component {
     render = () => {
         return (
-            <ResponsiveEmbed aspectRatio='16by9' frameBorder='false'>
-                <iframe src='https://itch.io/embed-upload/2958632?color=333333'
-                        allowfullscreen=''
-                        width='960' height='600' frameborder='0'
-                />
-            </ResponsiveEmbed>
+            <div>hello</div>
         )
     }
 }
+// removed and added it to its own seperate component for later use!
+const itchEmbeddedFrame = () => {
+  return (
+    <ResponsiveEmbed aspectRatio='16by9' frameBorder='false'>
+      <iframe src='https://itch.io/embed-upload/2958632?color=333333'
+              allowfullscreen=''
+              width='960' 
+              height='600' 
+              frameborder='0'
+      />
+    </ResponsiveEmbed>
+  )
+}
+
 export default UnityComponent;
